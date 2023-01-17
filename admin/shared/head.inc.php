237398e41a31744c461049d4657b5e2ece80fb22
Marcel Jurtz <jurtzmarcel@gmail.com>
2023-01-17 21:01:34 +0100

    Add viewport data to header, closes #7

--- admin/shared/head.inc.php
+++ admin/shared/head.inc.php
@@ -5,6 +5,7 @@
 <head>
     <meta charset="utf-8">
     <title>WOD Manager</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo $root .  '/css/stylesheet.css' ?>" type="text/css">
     <link rel="stylesheet" href="<?php echo $root .  '/vendor/fontawesome-free-6.2.0-web/css/all.min.css' ?>" type="text/css">
     <link rel="stylesheet" href="<?php echo $root .  '/vendor/bootstrap-5.2.1-dist/css/bootstrap.min.css' ?>" type="text/css">
